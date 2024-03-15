5e3e21b1d11a1f833d4a143f480b16c60fe73bdd
Edwin's <155349594+Edwin0305x@users.noreply.github.com>
2024-03-15 21:43:18 +0700

    a10s : initial commit

---
+++ device.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/a10s
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
